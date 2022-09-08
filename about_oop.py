7f025d3ce8ee3289b8b538c81c3a2cae5196df7f
Vitalik <75811667+lkdrm@users.noreply.github.com>
2022-09-08 22:00:21 +0200

    Continue to read how with OOP make tasks

--- about_oop.py
+++ about_oop.py
@@ -1,5 +1,6 @@
 # from about_os import Car
-from square import Square,RegularSqrPrisma
+
+from square import Square,RegularSqrPrisma,Triangle,EquilateralTriangle
 
 # first_car = Car(2015,"Mercedes-benz","GLC")
 
@@ -19,3 +20,21 @@ print(f"The square with max is : {max_diag_square.print_info()}")
 
 print("*"*50)
 print(f"The min of prisma is : {min_prisma.print_prisma()}")
+
+the_triangle = [Triangle(2,5,6), Triangle(5,2,4), Triangle(7,3,6)]
+the_equal_triangle = [EquilateralTriangle(5), EquilateralTriangle(8), EquilateralTriangle(6),EquilateralTriangle(4)]
+
+# print(the_triangle[0].print_triangle())
+# print(the_equal_triangle[0].exists())
+# the_triangle[0].get_angles()
+# the_triangle[0].get_perimetr()
+# the_triangle[0].get_square()
+
+triangle_s = list(map(lambda triangle : triangle.get_square(), the_triangle))
+triangle_s_avg = sum(triangle_s) / len(triangle_s)
+
+equila_triangle_maximum_p = max(the_equal_triangle, key = lambda triangle: triangle.get_perimetr())
+
+print("*"*50)
+print(f"The average triangles square: {triangle_s_avg:.2f}")
+print(f"The equilateral triangle with the max perimeter is : {equila_triangle_maximum_p.print_triangle()}")
--- square.py
+++ square.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, degrees, acos
 
 class Square:
     def __init__(self,side:int):
@@ -35,3 +35,47 @@ class RegularSqrPrisma(Square):
 
     def print_prisma(self):
         return f"Regular square prisma is : base side -- {self.side} & height is -- {self.height}"
+
+class Triangle():
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def print_triangle(self):
+        return f"ABC (A-{self.a}, B-{self.b}, C-{self.c})"
+    
+    def exists(self):
+        if(self.a + self.b > self.c) and (self.a + self.c > self.b) and (self.b + self.c > self.a):
+            return f"{self.print_triangle()} - is exist"
+        else:
+            return f"{self.print_triangle()} - doesn`t exist"
+    
+    def print_sides(self):
+        return f"Sides : {self.a}, {self.b}, {self.c}"
+    
+    def get_angles(self):
+        cos_CAB = acos((self.a**2 + self.c**2 - self.b**2)/(2*self.a * self.c))
+        cos_ABC = acos((self.a**2 + self.b**2 - self.c**2)/(2*self.a * self.b))
+        cos_BCA = acos((self.b**2 + self.c**2 - self.a**2)/(2*self.b * self.c))
+
+        print(f"CAB: {cos_CAB:.2f} Rad, {degrees(cos_CAB):.2f} Grad")
+        print(f"ABC: {cos_ABC:.2f} Rad, {degrees(cos_ABC):.2f} Grad")
+        print(f"BCA: {cos_BCA:.2f} Rad, {degrees(cos_BCA):.2f} Grad")
+    
+    def get_perimetr(self):
+        p = self.a + self.b + self.c
+        print(f"Perimetr is: {p}.")
+        return p
+
+    def get_square(self):
+        p = (self.a + self.b + self.c)/2
+        s = sqrt(p * ((p-self.a) * (p-self.b) * (p-self.c)))
+        print(f"The square is: {s:.2f}")
+        return s
+
+class EquilateralTriangle(Triangle):
+    def __init__(self, a,):
+        super().__init__(a,a,a)
+
+#inheritance
